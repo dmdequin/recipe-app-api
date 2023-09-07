311081644e4b99d8a5b828f91a6d3a4216be639e
Danielle Dequin <dmdequin@gmail.com>
2023-09-07 09:18:03 +0200

    added the wait_for_db script command

--- app/core/tests/test_commands.py
+++ app/core/tests/test_commands.py
@@ -10,27 +10,26 @@ from django.db.utils import OperationalError
 from django.test import SimpleTestCase
 
 
-# patch is the mocked object
 @patch('core.management.commands.wait_for_db.Command.check')
 class CommandTests(SimpleTestCase):
     """Tests commands."""
 
-    def test_wait_for_db_read(self, patched_check):
+    def test_wait_for_db_ready(self, patched_check):
         """Test waiting for database if database ready."""
         patched_check.return_value = True
 
         call_command('wait_for_db')
 
         # asserts the mocked values are called with these parameters
-        patched_check.assert_called_once_with(database=['default'])
+        patched_check.assert_called_once_with(databases=['default'])
 
     @patch('time.sleep')
     def test_wait_for_db_delay(self, patched_sleep, patched_check):
         """Test waiting for database when getting operational error."""
         patched_check.side_effect = [Psycopg2Error] * 2 + \
             [OperationalError] * 3 + [True]
-        
+
         call_command('wait_for_db')
 
         self.assertEqual(patched_check.call_count, 6)
-        patched_check.assert_called_with(database=['default'])
+        patched_check.assert_called_with(databases=['default'])
